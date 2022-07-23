6ee2f557efa4c67d68a8ee75ab2ba16bff5532be
Eoan O Dea <odeaeoan@gmail.com>
2022-07-23 19:14:11 +0300

    Integration with existing microsservice
    -Completed integration with existing microservice
    -Added further error handling
    -Response to client with new string output with tags replaced
    -Added .env and example.env for dynamic environment variables
    -Added cors origin for testing

--- index.js
+++ index.js
@@ -1,30 +1,77 @@
 import express from "express";
+import axios from "axios";
+import dotenv from "dotenv";
+import cors from "cors";
+dotenv.config();
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.use(express.json());
 
+var corsOptions = {
+  origin: process.env.CORS_ORIGIN,
+  optionsSuccessStatus: 200, // For legacy browser support
+};
+
+app.use(cors(corsOptions));
+
 const handleResponse = (res, status, message) => {
-  res.status(status).send({ success: status === 200, message });
+  res.status(status).send(message);
 };
 
 app.post("/resolveendpoint", (req, res) => {
   const { input } = req.body;
+
   const rgx = new RegExp(/(\$\([a-z]{4,}:[0-9]\))/g);
 
   if (typeof input == "string" && rgx.test(input)) {
     const matches = input.match(rgx);
-    console.log(matches);
+    let output = input;
 
-    return handleResponse(res, 200, "Good job");
+    matches.forEach((match, index) => {
+      const [name, value] = match.split(/[$()]/)[2].split(":");
+
+      fetchTag(name, value)
+        .then((response) => {
+          output = output.replace(match, response.data);
+
+          if (index === matches.length - 1) {
+            return handleResponse(res, 200, output);
+          }
+        })
+        .catch(() => {
+          return handleResponse(
+            res,
+            500,
+            "The server encountered and error, please try again later"
+          );
+        });
+    });
+  } else {
+    return handleResponse(
+      res,
+      400,
+      "Input must be a string and contain at least one tag e.g. 'Hello $(firstname:1)'"
+    );
   }
-  return handleResponse(
-    res,
-    400,
-    "Input must be a string and contain at least one tag e.g. 'Hello ${firstname:1}'"
-  );
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Template Engine listening on port ${port}`);
 });
+
+const fetchTag = (name, value) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const response = await axios.get(
+        `${process.env.EXISTING_MICROSERVICE_URL}/template/${name}/${value}`
+      );
+
+      resolve(response);
+    } catch (err) {
+      console.log("Error fetching tag", err);
+      reject(err);
+    }
+  });
+};

97dcf0c1d345bef48e71e6e2b73e305e5d659275
Eoan O Dea <odeaeoan@gmail.com>
2022-07-23 18:16:42 +0300

    Implementation & server setup
    -Implemented start and dev scripts in package.json
    -Added cors and nodemon dependencies
    -Added resolveendpoint endpoint and regex to match incoming strings
    -Added initial error handling

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-const express = require("express");
+import express from "express";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(express.json());
+
+const handleResponse = (res, status, message) => {
+  res.status(status).send({ success: status === 200, message });
+};
+
+app.post("/resolveendpoint", (req, res) => {
+  const { input } = req.body;
+  const rgx = new RegExp(/(\$\([a-z]{4,}:[0-9]\))/g);
+
+  if (typeof input == "string" && rgx.test(input)) {
+    const matches = input.match(rgx);
+    console.log(matches);
+
+    return handleResponse(res, 200, "Good job");
+  }
+  return handleResponse(
+    res,
+    400,
+    "Input must be a string and contain at least one tag e.g. 'Hello ${firstname:1}'"
+  );
 });
 
 app.listen(port, () => {
